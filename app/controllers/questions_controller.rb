8eac593b997cded6a211fa9e1a513cbf2257c406
Ahmed Menouni <ahmed.menouni@gmail.com>
2018-08-13 13:43:56 +0200

    implementing coach answer logic

---
+++ app/controllers/questions_controller.rb
@@ -0,0 +1,29 @@
+class QuestionsController < ApplicationController
+
+  #  QUESTIONS = {
+  #   1 => { question: "How are you?" },
+  #   2 => { question: "What's the weather?" }
+  # }
+
+  def ask
+  end
+
+  def answer
+    @question = params[:question]
+    @answer = coach_answer(@question)
+    # @questions = QUESTIONS
+  end
+
+  private
+
+  def coach_answer(your_message)
+    # TODO: return coach answer to your_message
+    if your_message.include? "?"
+      return "Silly question, get dressed and go to work!"
+    elsif your_message == "I am going to work right now!"
+      return ""
+    else
+      return "I don't care, get dressed and go to work!"
+    end
+  end
+end
